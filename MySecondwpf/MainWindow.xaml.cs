b6b6ac596925891a5da3e5cf2259817950875420
ROS7-PC <ROS7-PC@DESKTOP-5PGRS8R>
2018-11-19 16:22:47 +0530

    Add project files.

---
+++ MySecondwpf/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace MySecondwpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            binddatagrid();
+        }
+
+        private void binddatagrid()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["connsudent"].ConnectionString;
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select * from [student2]";
+            cmd.Connection = con;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable("student2");
+            da.Fill(dt);
+
+            g1.ItemsSource = dt.DefaultView;
+            con.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["connsudent"].ConnectionString;
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "insert into [student2](name)values(@nm) ";
+            cmd.Parameters.AddWithValue("@nm",text1.Text);
+            cmd.Connection = con;
+            int a = cmd.ExecuteNonQuery();
+            if (a == 1)
+            {
+                MessageBox.Show("Data Inserted");
+                binddatagrid();
+            }
+
+        }
+
+        
+    }
+}
